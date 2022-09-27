00ea5703df3e8e2ecca67900ddbc9e9b75be279c
Jonathan Core <56272039+JCoreMS@users.noreply.github.com>
2022-09-27 11:19:10 -0400

    Removing Scripts Folder - not needed
    These scripts were optional and not referenced or part of the deployment.

--- Deployments/AppService-WebApp/scripts/get-webAppPublishInfo.ps1
+++
@@ -1,4 +0,0 @@
-$webappName = "fta-aznamingtool"
-$PublishFile = "c:\temp\aznamingToolPubFile.txt"
-
-Get-AzWebApp -Name $webappName | Get-AzWebAppPublishingProfile -OutputFile $PublishFile | Out-Null
